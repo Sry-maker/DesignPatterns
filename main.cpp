30a5844a34cc08309073f9b4866519dd03ec2224
Haotian Wu <3233990645@qq.com>
2021-10-14 21:35:36 +0800

    主函数

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DesignPatterns
+//
+//  Created by 吴昊天 on 2021/10/9.
+//
+
+#include "FactoryMethod/factoryMethod.h"
+#include "AbstractFactory/abstractFactory.h"
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    factoryMethod();
+    abstractFactory();
+    return 0;
+}
