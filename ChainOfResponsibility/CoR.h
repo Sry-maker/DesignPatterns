075381d8d981364a491a6fc370829fef889c2843
1602347312 <83688154+1602347312@users.noreply.github.com>
2021-10-16 20:13:05 +0800

    Add files via upload

---
+++ ChainOfResponsibility/CoR.h
@@ -0,0 +1,59 @@
+﻿#pragma once
+/**
+* The audience class represents people who are the audience of the Olympic Games.
+*/
+class audience {
+public:
+    std::string ID;
+    std::string name;
+    std::string ticketID;
+    bool dangerous_articles;//whether the person carries dangerous articles.
+
+    audience(std::string ID, std::string name, std::string ticketID, bool dangerous_articles)
+    {
+        this->ID = ID;
+        this->name = name;
+        this->ticketID = ticketID;
+        this->dangerous_articles = dangerous_articles;
+    }
+};
+
+
+
+/**
+ * The Handler interface declares a method for building the chain of handlers.
+ * It also declares a method for executing a request.
+ */
+class Handler {
+public:
+    virtual Handler* SetNext(Handler* handler) = 0;
+    virtual void Handle(audience request) = 0;
+};
+/**
+ * The default chaining behavior can be implemented inside a base handler class.
+ */
+class AbstractHandler : public Handler {
+    /**
+     * @var Handler
+     */
+private:
+    Handler* next_handler_;
+
+public:
+    AbstractHandler() : next_handler_(nullptr) {
+    }
+    Handler* SetNext(Handler* handler) override {
+        this->next_handler_ = handler;
+        // Returning a handler from here will let us link handlers in a convenient
+        // way
+        return handler;
+    }
+    void Handle(audience request) override {
+        
+        if (this->next_handler_) {
+            this->next_handler_->Handle(request);
+        }
+
+        
+    }
+};
---
+++ ChainOfResponsibility/chainOfResponsibility.h
@@ -0,0 +1,91 @@
+﻿#include<iostream>
+#include<string>
+#include<vector>
+#include"CoR.h"
+
+
+//身份核验-安检-验票
+
+
+/**
+ * All Concrete Handlers either handle a request or pass it to the next handler
+ * in the chain.
+ */
+class IdentityAuthenticationHandler : public AbstractHandler {
+public:
+    void Handle(audience request) override {
+        if (request.ID == "1"&&request.name=="1") {
+            std::cout << "身份核验通过" << std::endl;
+            AbstractHandler::Handle(request);
+        }
+        else {
+            std::cout << "身份核验不通过" << std::endl;
+        }
+        
+    }
+};
+class SecurityCheckHandler : public AbstractHandler {
+public:
+    void Handle(audience request) override {
+        if (request.dangerous_articles == false) {
+            std::cout << "安检通过" << std::endl;
+            AbstractHandler::Handle(request);
+        }
+        else {
+            std::cout << "安检不通过" << std::endl;
+        }
+        
+    }
+};
+class TicketCheckHandler : public AbstractHandler {
+public:
+    void Handle(audience request) override {
+        if (request.ticketID == "1") {
+            std::cout << "验票通过" << std::endl;
+            AbstractHandler::Handle(request);
+        }
+        else {
+            std::cout << "验票不通过" << std::endl;
+        }
+        
+    }
+};
+/**
+ * The client code is usually suited to work with a single handler. In most
+ * cases, it is not even aware that the handler is part of a chain.
+ */
+void ClientCode(Handler& handler) {
+    std::vector<audience> audiences = { {"1", "1", "1", 0 } ,{"2","1","1",0} ,{"1","1","2",0} ,{"1","1","1",1} };
+    int count = 0;
+    for (const audience& i : audiences) {
+        count += 1;
+        std::cout << std::endl << std::endl << "第" << count << "位观众正在入场" << std::endl << std::endl;
+        handler.Handle(i);
+        
+    }
+}
+/**
+ * The other part of the client code constructs the actual chain.
+ */
+void chainOfResponsibilityTest() {
+    IdentityAuthenticationHandler* IdentityAuthentication = new IdentityAuthenticationHandler;
+    SecurityCheckHandler* SecurityCheck = new SecurityCheckHandler;
+    TicketCheckHandler* TicketCheck = new TicketCheckHandler;
+    IdentityAuthentication->SetNext(SecurityCheck)->SetNext(TicketCheck);
+
+    /**
+     * The client should be able to send a request to any handler, not just the
+     * first one in the chain.
+     */
+    std::cout << "本程序模拟奥林匹克运动会观众身份核验-安检-验票入场场景，使用了CoR--责任链模式" << std::endl << std::endl;
+    std::cout << "观众开始入场" << std::endl << std::endl;
+    ClientCode(*IdentityAuthentication);
+    
+
+    delete IdentityAuthentication;
+    delete SecurityCheck;
+    delete TicketCheck;
+
+    std::cout << std::endl << "***************  CoR(责任链)设计模式结束  **************************************" << std::endl << std::endl;
+
+}
