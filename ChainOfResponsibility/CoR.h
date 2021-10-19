9eb88c3eccb96216a193d7ee05b99ed69de65829
1602347312 <83688154+1602347312@users.noreply.github.com>
2021-10-19 18:27:48 +0800

    Update CoR.h

--- ChainOfResponsibility/CoR.h
+++ ChainOfResponsibility/CoR.h
@@ -33,21 +33,22 @@ public:
  * The default chaining behavior can be implemented inside a base handler class.
  */
 class AbstractHandler : public Handler {
-    /**
-     * @var Handler
-     */
 private:
     Handler* next_handler_;
 
 public:
     AbstractHandler() : next_handler_(nullptr) {
     }
+    
+    //设置chain,连接下一个handler
     Handler* SetNext(Handler* handler) override {
         this->next_handler_ = handler;
         // Returning a handler from here will let us link handlers in a convenient
         // way
         return handler;
     }
+    
+    //向下一个handler传递request
     void Handle(audience request) override {
         
         if (this->next_handler_) {
