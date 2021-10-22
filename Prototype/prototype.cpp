d382a80671940517ecb0265cba844628f633b590
xiaochao <1961384758@qq.com>
2021-10-22 15:35:40 +0800

    hnc

--- Prototype/prototype.cpp
+++ Prototype/prototype.cpp
@@ -5,148 +5,150 @@ using namespace std;
 
 //场地运动规范 足球、乒乓球、冰球     球门、球网、球拍规范、护具、球杆
 enum Type {
-    Football = 0,//足球
-    Pingpong,    //乒乓球
-    Icehockey    //冰球
+	Football = 0,//足球
+	Pingpong,    //乒乓球
+	Icehockey    //冰球
 
 };
 
 
 class Prototype {//原型
 protected:
-    string prototypename; //运动项目
-    double length;        //场地长度
-    double width;         //场地宽度
-     
+	string prototypename; //运动项目
+	double length;        //场地长度
+	double width;         //场地宽度
+
 
 public:
-    Prototype() {}//构造函数
-    Prototype(string prototype_name) {
-        this->prototypename = prototype_name;
-    }
-    virtual ~Prototype() {}//析构函数
-    virtual Prototype* Clone() const = 0;//根据原型构造新型
-    virtual void assign(double length,double width) {//赋值
-        this->length = length;
-        this->width = width;
-    }
-    virtual void standard() const = 0;//规范打印
+	Prototype() {}//构造函数
+	Prototype(string prototype_name) {
+		this->prototypename = prototype_name;
+	}
+	virtual ~Prototype() {}//析构函数
+	virtual Prototype* Clone() const = 0;//根据原型构造新型
+	virtual void assign(double length, double width) {//赋值
+		this->length = length;
+		this->width = width;
+	}
+	virtual void standard() const = 0;//规范打印
 };
 
 
 class FootballPrototype : public Prototype {//足球场地
 private:
-    string goalstandard;//足球球门规范
+	string goalstandard;//足球球门规范
 
 public:
-    FootballPrototype(string prototype_name, string goal_standard)//构造函数
-        : Prototype(prototype_name), goalstandard(goal_standard) {
-    }
+	FootballPrototype(string prototype_name, string goal_standard)//构造函数
+		: Prototype(prototype_name), goalstandard(goal_standard) {
+	}
 
-    Prototype* Clone() const override {//构造新型
-        return new  FootballPrototype(*this);
-    }
-    void standard() const override {//规范打印
-        cout << prototypename << ":" << endl;
-        cout << "场地长度:" << length << endl << "场地宽度:" << width << endl << "足球球门规范:" << goalstandard << endl << endl;
-    }
+	Prototype* Clone() const override {//构造新型
+		return new  FootballPrototype(*this);
+	}
+	void standard() const override {//规范打印
+		cout << "FootballPrototype::standard : " << endl << prototypename << ":" << endl;
+		cout << "场地长度:" << length << endl << "场地宽度:" << width << endl << "足球球门规范:" << goalstandard << endl << endl;
+	}
 };
 
 
 class PingpongPrototype : public Prototype {//乒乓场地
 private:
-    string netstandard;//球网规范
-    string racketstandard;//球拍规范
+	string netstandard;//球网规范
+	string racketstandard;//球拍规范
 
 public:
-    PingpongPrototype(string prototype_name, string net_standard, string racket_standard)//构造函数
-        : Prototype(prototype_name), netstandard(net_standard), racketstandard(racket_standard) {
-    }
+	PingpongPrototype(string prototype_name, string net_standard, string racket_standard)//构造函数
+		: Prototype(prototype_name), netstandard(net_standard), racketstandard(racket_standard) {
+	}
 
-    Prototype* Clone() const override {//构造新型
-        return new  PingpongPrototype(*this);
-    }
-    void standard() const override {//规范打印
-        cout << prototypename << ":" << endl;
-        cout << "场地长度:" << length << endl << "场地宽度:" << width << endl << "乒乓球球网规范:" << netstandard<<endl<<"乒乓球球拍规范:"<<racketstandard << endl << endl;
-    }
+	Prototype* Clone() const override {//构造新型
+		return new  PingpongPrototype(*this);
+	}
+	void standard() const override {//规范打印
+		cout << "PingpongPrototype::standard : " << endl << prototypename << ":" << endl;
+		cout << "场地长度:" << length << endl << "场地宽度:" << width << endl << "乒乓球球网规范:" << netstandard << endl << "乒乓球球拍规范:" << racketstandard << endl << endl;
+	}
 };
 
 class icehockeyPrototype : public Prototype {//冰球场地
 private:
-    string goalstandard;//球门规范
-    string clubstandard;//球杆规范
+	string goalstandard;//球门规范
+	string clubstandard;//球杆规范
 
 public:
-    icehockeyPrototype(string prototype_name, string goal_standard,string club_standard)//构造函数
-        : Prototype(prototype_name), goalstandard(goal_standard), clubstandard(club_standard){
-    }
+	icehockeyPrototype(string prototype_name, string goal_standard, string club_standard)//构造函数
+		: Prototype(prototype_name), goalstandard(goal_standard), clubstandard(club_standard) {
+	}
 
-    Prototype* Clone() const override {//构造新型
-        return new  icehockeyPrototype(*this);
-    }
-    void standard() const override {//规范打印
-        cout << prototypename << ":" << endl;
-        cout << "场地长度:" << length << endl << "场地宽度:"<<width << endl << "冰球球网规范:" << goalstandard << endl << "冰球球杆规范:" << clubstandard << endl << endl;
-    }
+	Prototype* Clone() const override {//构造新型
+		return new  icehockeyPrototype(*this);
+	}
+	void standard() const override {//规范打印
+		cout << "icehockeyPrototype::standard : " << endl << prototypename << ":" << endl;
+		cout << "场地长度:" << length << endl << "场地宽度:" << width << endl << "冰球球网规范:" << goalstandard << endl << "冰球球杆规范:" << clubstandard << endl << endl;
+	}
 };
 
 
 class PrototypeFactory {//原型
 private:
-    std::unordered_map<Type, Prototype*, std::hash<int>> prototypes_;
+	std::unordered_map<Type, Prototype*, std::hash<int>> prototypes_;
 
 public:
-    PrototypeFactory() {//构造函数，三个具体原型
-        prototypes_[Type::Football] = new FootballPrototype("足球规范", "足球门应设在每条球门线的中央，由两根相距7.32米、与西面角旗点相等距离 ......");
-        prototypes_[Type::Pingpong] = new PingpongPrototype("乒乓球规范 ", "球网装置包括球网、悬网绳、网柱及将它们固定在球台上的夹钳部分......","球拍的大小，形状和重量不限，但底板应平整、坚硬......");
-        prototypes_[Type::Icehockey] = new PingpongPrototype("冰球球规范 ", "冰球项目中，球门的宽度是180厘米，就是1米8宽......", "用木质材料制成，从根部至杆柄端不能长于147厘米......");
-    }
+	PrototypeFactory() {//构造函数，三个具体原型
+		prototypes_[Type::Football] = new FootballPrototype("足球规范", "足球门应设在每条球门线的中央，由两根相距7.32米、与西面角旗点相等距离 ......");
+		prototypes_[Type::Pingpong] = new PingpongPrototype("乒乓球规范 ", "球网装置包括球网、悬网绳、网柱及将它们固定在球台上的夹钳部分......", "球拍的大小，形状和重量不限，但底板应平整、坚硬......");
+		prototypes_[Type::Icehockey] = new PingpongPrototype("冰球球规范 ", "冰球项目中，球门的宽度是180厘米，就是1米8宽......", "用木质材料制成，从根部至杆柄端不能长于147厘米......");
+	}
 
 
-    ~PrototypeFactory() {//析构函数
-        delete prototypes_[Type::Football];
-        delete prototypes_[Type::Pingpong]; 
-        delete prototypes_[Type::Icehockey];
-    }
+	~PrototypeFactory() {//析构函数
+		delete prototypes_[Type::Football];
+		delete prototypes_[Type::Pingpong];
+		delete prototypes_[Type::Icehockey];
+	}
 
-    Prototype* CreatePrototype(Type type) {//返回某个具体原型
-        return prototypes_[type]->Clone();
-    }
+	Prototype* CreatePrototype(Type type) {//返回某个具体原型
+		return prototypes_[type]->Clone();
+	}
 
 };
 
 void Client(PrototypeFactory& prototype_factory) {//测试函数
-    
-    //测试足球
-    Prototype* prototype = prototype_factory.CreatePrototype(Type::Football);
-    prototype->assign(105,70);
-    prototype->standard();
-    delete prototype;
 
-    cout << endl;
-    //测试乒乓球
-    prototype = prototype_factory.CreatePrototype(Type::Pingpong);
-    prototype->assign(2.7,1.5);
-    prototype->standard();
-    delete prototype;
-    
-    cout << endl;
-    //测试冰球
-    prototype = prototype_factory.CreatePrototype(Type::Icehockey);
-    prototype->assign(61, 30);
-    prototype->standard();
-    delete prototype;
+	//测试足球
+	Prototype* prototype = prototype_factory.CreatePrototype(Type::Football);
+	prototype->assign(105, 70);
+	prototype->standard();
+	delete prototype;
+
+	cout << endl;
+	//测试乒乓球
+	prototype = prototype_factory.CreatePrototype(Type::Pingpong);
+	prototype->assign(2.7, 1.5);
+	prototype->standard();
+	delete prototype;
+
+	cout << endl;
+	//测试冰球
+	prototype = prototype_factory.CreatePrototype(Type::Icehockey);
+	prototype->assign(61, 30);
+	prototype->standard();
+	delete prototype;
 }
 
-void prototypeTest() 
+void prototypeTest()
 {
-    PrototypeFactory* prototype_factory = new PrototypeFactory();
-    Client(*prototype_factory);
-    delete prototype_factory;
+	cout << "*****************Prototype(原型)设计模式**********" << endl << endl;
+	PrototypeFactory* prototype_factory = new PrototypeFactory();
+	Client(*prototype_factory);
+	delete prototype_factory;
+	cout << "**************************************************" << endl << endl;
 }
 int main() {
-  
-    prototypeTest();
-    return 0;
+
+	prototypeTest();
+	return 0;
 }
--- Singleton/singleton.cpp
+++ Singleton/singleton.cpp
@@ -8,42 +8,42 @@ class Singleton
 {
 
 private:
-    static Singleton* pinstance;
-    static mutex mut;     //线程安全
+	static Singleton* pinstance;
+	static mutex mut;     //线程安全
 
 protected:
-    string Olympicname;   //奥林匹克运动会项目
-    string recordholder;  //记录保持者
-    string record;        //记录
-    Singleton(const string name,const string holder,const string rec) : Olympicname(name), recordholder(holder), record(rec)//构造函数
-    {
-    }
-    ~Singleton() {}//析构函数
-  
+	string Olympicname;   //奥林匹克运动会项目
+	string recordholder;  //记录保持者
+	string record;        //记录
+	Singleton(const string name, const string holder, const string rec) : Olympicname(name), recordholder(holder), record(rec)//构造函数
+	{
+	}
+	~Singleton() {}//析构函数
+
 
 public:
 
-    Singleton(Singleton& other) = delete;  //单例模式不可以进行的操作
-    void operator=(const Singleton&) = delete;
+	Singleton(Singleton& other) = delete;  //单例模式不可以进行的操作
+	void operator=(const Singleton&) = delete;
 
-    static Singleton* GetInstance(const string& name, const string& holder, const string& rec);//获取实例
-
-
-    void recorddeclaration()   //记录打印
-    {
-        cout << "奥林匹克运动会项目: " << Olympicname << endl << "纪录保持者: " << recordholder << endl << "记录: " << record << endl;
-    }
+	static Singleton* GetInstance(const string& name, const string& holder, const string& rec);//获取实例
 
 
-    string Olympicnamevalue() const {//返回奥林匹克运动会项目
-        return Olympicname;
-    }
-    string recordholdervalue() const {//返回纪录保持者
-        return recordholder;
-    }
-    string recordvalue() const {//返回记录
-        return record;
-    }
+	void recorddeclaration()   //记录打印
+	{
+		cout << "Singleton::recorddeclaration : " << endl << "奥林匹克运动会项目: " << Olympicname << endl << "纪录保持者: " << recordholder << endl << "记录: " << record << endl;
+	}
+
+
+	string Olympicnamevalue() const {//返回奥林匹克运动会项目
+		return Olympicname;
+	}
+	string recordholdervalue() const {//返回纪录保持者
+		return recordholder;
+	}
+	string recordvalue() const {//返回记录
+		return record;
+	}
 };
 
 
@@ -53,29 +53,34 @@ mutex Singleton::mut;
 
 Singleton* Singleton::GetInstance(const string& name, const string& holder, const string& rec)//获取实例
 {
-    lock_guard<std::mutex> lock(mut);
-    if (pinstance == nullptr)//如果为nullptr则创建，否则直接返回已经创建好的
-    {
-        pinstance = new Singleton(name,  holder, rec);
-    }
-    return pinstance;
+	lock_guard<std::mutex> lock(mut);
+	if (pinstance == nullptr)//如果为nullptr则创建，否则直接返回已经创建好的
+	{
+		pinstance = new Singleton(name, holder, rec);
+	}
+	return pinstance;
 }
 
 void singletonTest() {//测试函数
-    cout << "记录创建" << endl;
-    Singleton* singleton = Singleton::GetInstance("男子100米", "博尔特", "9秒58");
-    singleton->recorddeclaration();
 
-    cout << endl << endl << endl;
-    cout << "记录查询" << endl;
-    Singleton* singl = Singleton::GetInstance("", "", "");
-    singl->recorddeclaration();
-    
+
+	cout << "*****************Singleton(单例)设计模式**********" << endl << endl;
+
+	cout << "记录创建" << endl;
+	Singleton* singleton = Singleton::GetInstance("男子100米", "博尔特", "9秒58");
+	singleton->recorddeclaration();
+
+	cout << endl << endl << endl;
+	cout << "记录查询" << endl;
+	Singleton* singl = Singleton::GetInstance("", "", "");
+	singl->recorddeclaration();
+
+	cout << "**************************************************" << endl << endl;
 }
 
 int main()
 {
 
-    singletonTest();
-    return 0;
+	singletonTest();
+	return 0;
 }
