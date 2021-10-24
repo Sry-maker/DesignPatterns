e5fef380b6183561142bb0a1e3dbee5f0bd43dd7
511tony <86397089+511tony@users.noreply.github.com>
2021-10-24 15:57:07 +0800

    输出

--- Facade/facade.h
+++ Facade/facade.h
@@ -1,35 +1,48 @@
 #pragma once
 #include <iostream>
 using namespace std;
+/***************************************************************************
+  类  名称：Fencing
+  功    能：用来完成击剑比赛
+  说    明：
+***************************************************************************/
 class Fencing {
 public:
-	std::string Operation1() const {
-		return "录入击剑比赛选手信息!\n";
+	std::string enterAthlete() const {
+		return "Fencing::enterAthlete:录入击剑比赛选手信息!\n";
 	}
-	std::string Operation2() const {
-		return "生成击剑比赛!\n";
+	std::string generateCompetition() const {
+		return "Fencing::generateCompetition:生成击剑比赛!\n";
 	}
 	// ...
-	std::string OperationN() const {
-		return "比赛完成，记录名次!\n";
+	std::string settlement() const {
+		return "Fencing::settlement:比赛完成，记录名次!\n";
 	}
 };
-
+/***************************************************************************
+  类  名称：Swimming
+  功    能：用来完成游泳比赛
+  说    明：
+***************************************************************************/
 class Swimming {
 public:
-	std::string Operation1() const {
-		return "录入游泳选手信息!\n";
+	std::string enterAthlete() const {
+		return "Swimming::enterAthlete:录入游泳选手信息!\n";
 	}
-	std::string Operation2() const {
-		return "生成游泳比赛!\n";
+	std::string generateCompetition() const {
+		return "Swimming::generateCompetition:生成游泳比赛!\n";
 	}
 	// ...
-	std::string OperationN() const {
-		return "比赛完成，记录名次!\n";
+	std::string settlement() const {
+		return "Swimming::settlement:比赛完成，记录名次!\n";
 	}
 };
 
-
+/***************************************************************************
+  类  名称：Facade
+  功    能：利用Facade完成比赛项目的初始化工作
+  说    明：Init 对所有项目进行初始化，只列举了游泳和击剑比赛
+***************************************************************************/
 class Facade {
 protected:
 	Fencing* fencing_;
@@ -49,11 +62,11 @@ public:
 	}
 	
 	std::string Init() {
-		std::string result = "Facade 初始化比赛项目:\n";
-		result += this->fencing_->Operation1();
-		result += this->swimming_->Operation1();
-		result += this->fencing_->Operation2();
-		result += this->swimming_->Operation2();
+		std::string result = "Facade::Init: 初始化比赛项目:\n";
+		result += this->fencing_->enterAthlete();
+		result += this->swimming_->enterAthlete();
+		result += this->fencing_->generateCompetition();
+		result += this->swimming_->generateCompetition();
 		return result;
 	}
 };
@@ -65,6 +78,7 @@ void Init(Facade* facade) {
 	// ...
 }
 int facadeTest() {
+	std::cout << "*************** facade模式展示:*************************************\n";
 	Fencing* fencing = new Fencing;
 	Swimming* swimming = new Swimming;
 	Facade* facade = new Facade(fencing, swimming);
--- Flyweight/flyweight.h
+++ Flyweight/flyweight.h
@@ -3,106 +3,125 @@
 #include < unordered_map >
 
 using namespace std;
-struct SharedState
+
+
+/***************************************************************************
+  类  名称：athleteSharedState
+  功    能：存储运动员的共享数据
+  说    明：运动员数据中国际和运动类型应该共享以减少存储空间
+***************************************************************************/
+struct athleteSharedState
 {
 	std::string nationality_;
 	std::string type_;
 	//...
-	SharedState(const std::string& nationality, const std::string& type)
+	athleteSharedState(const std::string& nationality, const std::string& type)
 		: nationality_(nationality), type_(type)
 	{
 	}
 
-	friend std::ostream& operator<<(std::ostream& os, const SharedState& ss)
+	friend std::ostream& operator<<(std::ostream& os, const athleteSharedState& ss)
 	{
 		return os << "[ " << ss.nationality_ << " , " << ss.type_ << " ]";
 	}
 };
-
-struct UniqueState
+/***************************************************************************
+  类  名称：athleteUniqueState
+  功    能：存储运动员的特有数据
+  说    明：这是每个运动员数据中无法共享的数据，只能单独存储
+***************************************************************************/
+struct athleteUniqueState
 {
 	std::string name_;
 	std::string number_;
 	//...
-	UniqueState(const std::string& name, const std::string& number)
+	athleteUniqueState(const std::string& name, const std::string& number)
 		: name_(name), number_(number)
 	{
 	}
 
-	friend std::ostream& operator<<(std::ostream& os, const UniqueState& us)
+	friend std::ostream& operator<<(std::ostream& os, const athleteUniqueState& us)
 	{
 		return os << "[ " << us.name_ << " , " << us.number_ << " ]";
 	}
 };
 
-
+/***************************************************************************
+  类  名称：Flyweight
+  功    能：存储各个不同的运动员的共享数据
+  说    明：这是所有运动员共享数据的集合，如果某个新添加运动员的共享数据在Flyweight里已存在，则直接利用
+            display 展示一个运动员的数据
+***************************************************************************/
 class Flyweight
 {
 private:
-	SharedState* shared_state_;
+	athleteSharedState* shared_state_;
 
 public:
-	Flyweight(const SharedState* shared_state) : shared_state_(new SharedState(*shared_state))
+	Flyweight(const athleteSharedState* shared_state) : shared_state_(new athleteSharedState(*shared_state))
 	{
 	}
-	Flyweight(const Flyweight& other) : shared_state_(new SharedState(*other.shared_state_))
+	Flyweight(const Flyweight& other) : shared_state_(new athleteSharedState(*other.shared_state_))
 	{
 	}
 	~Flyweight()
 	{
 		delete shared_state_;
 	}
-	SharedState* shared_state() const
+	athleteSharedState* shared_state() const
 	{
 		return shared_state_;
 	}
-	void Operation(const UniqueState& unique_state) const
+	void display(const athleteUniqueState& unique_state) const
 	{
-		std::cout << "Flyweight: 展示共享数据 (" << *shared_state_ << ") 私有数据 (" << unique_state << ")\n";
+		std::cout << "Flyweight::display: 展示共享数据 (" << *shared_state_ << ") 私有数据 (" << unique_state << ")\n";
 	}
 };
-
+/***************************************************************************
+  类  名称：FlyweightFactory
+  功    能：通过FlyweightFactory来利用Flyweight
+  说    明：GetFlyweight 如果某个新添加运动员的共享数据在Flyweight里已存在，则直接利用。否则新建一个新的Flyweight
+            ListFlyweights 展示所有已有的Flyweight
+  ***************************************************************************/
 class FlyweightFactory
 {
 
 private:
 	std::unordered_map<std::string, Flyweight> flyweights_;
 
-	std::string GetKey(const SharedState& ss) const
+	std::string GetKey(const athleteSharedState& ss) const
 	{
 		return ss.nationality_ + "_" + ss.type_;
 	}
 
 public:
-	FlyweightFactory(std::initializer_list<SharedState> share_states)
+	FlyweightFactory(std::initializer_list<athleteSharedState> share_states)
 	{
-		for (const SharedState& ss : share_states)
+		for (const athleteSharedState& ss : share_states)
 		{
 			this->flyweights_.insert(std::make_pair<std::string, Flyweight>(this->GetKey(ss), Flyweight(&ss)));
 		}
 	}
 
-	/**
-	 * Returns an existing Flyweight with a given state or creates a new one.
-	 */
-	Flyweight GetFlyweight(const SharedState& shared_state)
+	
+	Flyweight GetFlyweight(const athleteSharedState& shared_state)
 	{
 		std::string key = this->GetKey(shared_state);
 		if (this->flyweights_.find(key) == this->flyweights_.end())
 		{
-			std::cout << "FlyweightFactory: 找不到 flyweight建一个新的.\n";
+			std::cout << "FlyweightFactory::GetFlyweight: 找不到 flyweight建一个新的.\n";
 			this->flyweights_.insert(std::make_pair(key, Flyweight(&shared_state)));
 		}
 		else
 		{
-			std::cout << "FlyweightFactory: 重复利用已经有的 flyweight.\n";
+			std::cout << "FlyweightFactory::GetFlyweight: 重复利用已经有的 flyweight.\n";
 		}
 		return this->flyweights_.at(key);
 	}
 	void ListFlyweights() const
 	{
 		size_t count = this->flyweights_.size();
-		std::cout << "\nFlyweightFactory: 共有 " << count << " 个flyweight：\n";
+		std::cout << "\nFlyweightFactory::ListFlyweights: 共有 " << count << " 个flyweight：\n";
 		for (std::pair<std::string, Flyweight> pair : this->flyweights_)
 		{
 			std::cout << pair.first << "\n";
@@ -115,12 +134,13 @@ void AddAthleteToPoliceDatabase(
 	FlyweightFactory& ff, const std::string& name, const std::string& number,
 	const std::string& nationality, const std::string& type)
 {
-	std::cout << "\n添加一个运动员到数据库.\n";
+	
+	std::cout << "添加一个运动员到数据库.\n";
 	const Flyweight& flyweight = ff.GetFlyweight({ nationality, type });
-	flyweight.Operation({ name, number });
+	flyweight.display({ name, number });
 }
 int flyweightTest() {
-
+	std::cout << "*************** flyweight模式展示:*************************************";
 	FlyweightFactory* factory = new FlyweightFactory({ {"游泳", "中国"}, {"击剑", "中国"}, {"游泳", "美国"}, {"击剑", "美国"} });
 	factory->ListFlyweights();
 
@@ -134,7 +154,7 @@ int flyweightTest() {
 		"詹姆斯",
 		"33654",
 		"击剑",
-		"美国");
+		"法国");
 	factory->ListFlyweights();
 	delete factory;
 
