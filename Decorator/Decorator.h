c47c69240011c5b31deae72319673be15245840a
tan-log <76274551+tan-log@users.noreply.github.com>
2021-10-24 18:48:43 +0800

    Update pattern of Callback, Decorator and Component.

--- Decorator/Decorator.h
+++ Decorator/Decorator.h
@@ -4,33 +4,73 @@
 using namespace std;
 
 //抽象构件角色：给出一个抽象接口，以规范准备接收附加责任的对象。
-class Windsurfing {
+class Diving {
 public:
-    virtual ~Windsurfing() {}
-    virtual string Operation() const = 0; //返回物品描述
+    virtual ~Diving() {}
+    virtual string Operation() const = 0; //返回动作描述
 };
 
 //具体构件角色：定义一个将要接收附加责任的类
-class ConcreteWindsurfing : public Windsurfing {
+class Straight : public Diving {
 public:
-    //返回物品类型
+    Straight() {
+        cout << "Straight::Operation() : Creat a basic action 直体" << endl;
+    }
+    //返回动作类型
     string Operation() const override {
-        return "帆板\n";
+        return "直体";
     }
 };
 
+//具体构件角色：定义一个将要接收附加责任的类
+class Pike : public Diving {
+public:
+    Pike() {
+        cout << "Pike::Operation(): Creat a basic action 屈体" << endl;
+    }
+    //返回动作类型
+    string Operation() const override {
+        return "屈体";
+    }
+};
+
+//具体构件角色：定义一个将要接收附加责任的类
+class Tuck : public Diving {
+public:
+    Tuck() {
+        cout << "Tuck::Operation(): Creat a basic action 抱膝" << endl;
+    }
+    //返回动作类型
+    string Operation() const override {
+        return "抱膝";
+    }
+};
+
+//具体构件角色：定义一个将要接收附加责任的类
+class TAT : public Diving {
+public:
+    TAT() {
+        cout << "TAT::Operation(): Creat a basic action: 翻腾兼转体" << endl;
+    }
+    //返回动作类型
+    string Operation() const override {
+        return "翻腾兼转体";
+    }
+};
+
+
 //装饰角色：持有一个构件对象的实例，并定义一个与抽象构件接口一致的接口
-class Decorator : public Windsurfing {
+class Decorator : public Diving {
 
 protected:
-    Windsurfing* Windsurfing_;
+    Diving* Diving_;
 
 public:
-    Decorator(Windsurfing* Windsurfing) : Windsurfing_(Windsurfing) {
+    Decorator(Diving* Diving) : Diving_(Diving) {
     }
 
     string Operation() const override {
-        return this->Windsurfing_->Operation(); //返回子类中的物品描述
+        return this->Diving_->Operation(); //返回子类中的动作描述
     }
 };
 
--- Decorator/decoratorTest.h
+++ Decorator/decoratorTest.h
@@ -3,65 +3,107 @@
 #include"Decorator.h"
 using namespace std;
 
-//装饰器1
-class AntiSlipPaint  : public Decorator {
-    
+//装饰器1，向前起跳
+class Front : public Decorator {
+
 public:
 
-    AntiSlipPaint (Windsurfing* Windsurfing) : Decorator(Windsurfing) {
-    }
- 
-    //该装饰器在调用父类operation的基础上实现自己的操作
-    string Operation() const override {
-        return "加装防滑漆\n" + Decorator::Operation() ; //返回物品描述
-    }
+	Front(Diving* Diving) : Decorator(Diving) {
+		cout << "Front:: Create a motion 向前" << endl;
+	}
+
+	//该装饰器在调用父类operation的基础上实现自己的操作
+	string Operation() const override {
+		return "向前" + Decorator::Operation(); //返回动作描述
+	}
 };
-//装饰器2
-class Centerboard : public Decorator {
+//装饰器2，向后起跳
+class Back : public Decorator {
 public:
-    Centerboard(Windsurfing* Windsurfing) : Decorator(Windsurfing) {
-    }
+	Back(Diving* Diving) : Decorator(Diving) {
+		cout << "Back:: Create a motion 向后" << endl;
+	}
 
-    //该装饰器在调用父类operation的基础上实现自己的操作
-    string Operation() const override {
-        return "加装稳向板\n" + Decorator::Operation(); //返回物品描述
-    }
+	//该装饰器在调用父类operation的基础上实现自己的操作
+	string Operation() const override {
+		return "向后" + Decorator::Operation(); //返回动作描述
+	}
 };
 
 
-//装饰器3
-class Collinder  : public Decorator {
+//装饰器3:反身
+class Reverse : public Decorator {
 public:
-    Collinder (Windsurfing* Windsurfing) : Decorator(Windsurfing) {
-    }
+	Reverse(Diving* Diving) : Decorator(Diving) {
+		cout << "Reverse:: Create a motion 反身" << endl;
+	}
 
-    //该装饰器在调用父类operation的基础上实现自己的操作
-    string Operation() const override {
-        return "加装防滑绳\n" + Decorator::Operation(); //返回物品描述
-    }
+	//该装饰器在调用父类operation的基础上实现自己的操作
+	string Operation() const override {
+		return "反身" + Decorator::Operation(); //返回动作描述
+	}
+};
+//装饰器4，翻腾半周数
+class doubleAxel : public Decorator {
+public:
+	doubleAxel(Diving* Diving) : Decorator(Diving) {
+		cout << "doubleAxel:: Create a motion 翻腾三周半" << endl;
+	}
+
+	//该装饰器在调用父类operation的基础上实现自己的操作
+	string Operation() const override {
+		return "翻腾三周半" + Decorator::Operation(); //返回动作描述
+	}
+};
+//装饰器5，转体半周数
+class Tumble : public Decorator {
+public:
+	Tumble(Diving* Diving) : Decorator(Diving) {
+		cout << "Front:: Create a motion 转体1周半" << endl;
+	}
+
+	//该装饰器在调用父类operation的基础上实现自己的操作
+	string Operation() const override {
+		return "转体1周半" + Decorator::Operation(); //返回动作描述
+	}
 };
 
-void Operation(Windsurfing* Windsurfing) {
-    cout << "选手设备:\n" << Windsurfing->Operation(); //返回物品描述
+void Operation(Diving* Diving) {
+	cout << "选手动作:\n" << Diving->Operation(); //返回动作描述
 }
-
 int decoratorTest() {
-    
-    Windsurfing* simple = new ConcreteWindsurfing;
-    cout << "第一位选手设备检查报告:\n";
-    Operation(simple);
-    cout << "\n\n";
-    
-    Windsurfing* decorator1 = new AntiSlipPaint (simple);
-    Windsurfing* decorator2 = new Collinder (decorator1);
-    Windsurfing* decorator3 = new Centerboard(decorator2);
-    cout << "第二位选手设备检查报告:\n";
-    Operation(decorator3);
-    cout << "\n";
+	cout << "****************以下为Decorator（装饰）设计模式****************:\n";
+	Diving* simple1 = new Straight;//直体
+	Diving* simple2 = new Pike;//屈体
+	Diving* simple3 = new Tuck;//抱膝
+	Diving* simple4 = new TAT;//翻腾兼转体
+	Diving* decorator1 = new Tumble(simple1);//转体1周半
+	Diving* decorator2 = new doubleAxel(simple2);//翻腾三周半
+	Diving* decorator3 = new doubleAxel(decorator1);
+	Diving* decorator4 = new Reverse(decorator2);//反身翻腾三周半屈体
+	Diving* decorator5 = new Front(decorator3);//向前翻腾三周半转体1周半直体
+	Diving* decorator6 = new Back(decorator2);//向后翻腾三周半屈体
 
-    delete simple;
-    delete decorator1;
-    delete decorator2;
+	cout << "第一位选手动作:\n";
+	cout <<"Reverse::Operation():" <<decorator4->Operation();
+	cout << "\n";
+	cout << "第二位选手动作:\n";
+	cout << "Front::Operation():" << decorator5->Operation();
+	cout << "\n";
+	cout << "第三位选手动作:\n";
+	cout << "Back::Operation():" << decorator6->Operation();
+	cout << "\n";
+	cout << "****************Decorator（装饰）设计模式结束****************:\n";
+	delete simple1;
+	delete simple2;
+	delete simple3;
+	delete simple4;
+	delete  decorator1;
+	delete decorator2;
+	delete decorator3;
+	delete decorator4;
+	delete decorator5;
+	delete decorator6;
 
-    return 0;
+	return 0;
 }
