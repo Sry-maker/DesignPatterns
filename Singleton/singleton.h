b082715be8c79456ef46f3d3c3dcd277391d915f
xiaochao <1961384758@qq.com>
2021-10-26 15:00:17 +0800

    hnc

--- Singleton/singleton.h
+++ Singleton/singleton.h
@@ -62,20 +62,95 @@ Singleton* Singleton::GetInstance(const std::string& name, const std::string& ho
 	return pinstance;
 }
 
+
+
+class Singleton1
+{
+
+private:
+	static Singleton1* pinstance;
+	static std::mutex mut;     //线程安全
+
+protected:
+	std::string Olympicname;   //奥林匹克运动会项目
+	std::string recordholder;  //记录保持者
+	std::string record;        //记录
+	Singleton1(const std::string name, const std::string holder, const std::string rec) : Olympicname(name), recordholder(holder), record(rec)//构造函数
+	{
+	}
+	~Singleton1() {}//析构函数
+
+
+public:
+
+	Singleton1(Singleton1& other) = delete;  //单例模式不可以进行的操作
+	void operator=(const Singleton1&) = delete;
+
+	static Singleton1* GetInstance(const std::string& name, const std::string& holder, const std::string& rec);//获取实例
+
+
+	void recorddeclaration()   //记录打印
+	{
+		std::cout << "Singleton1::recorddeclaration : " << std::endl << "奥林匹克运动会项目: " << Olympicname << std::endl << "纪录保持者: " << recordholder << std::endl << "记录: " << record << std::endl;
+	}
+
+
+	std::string Olympicnamevalue() const {//返回奥林匹克运动会项目
+		return Olympicname;
+	}
+	std::string recordholdervalue() const {//返回纪录保持者
+		return recordholder;
+	}
+	std::string recordvalue() const {//返回记录
+		return record;
+	}
+};
+
+
+Singleton1* Singleton1::pinstance{ nullptr };
+std::mutex Singleton1::mut;
+
+
+Singleton1* Singleton1::GetInstance(const std::string& name, const std::string& holder, const std::string& rec)//获取实例
+{
+	std::lock_guard<std::mutex> lock(mut);
+	if (pinstance == nullptr)//如果为nullptr则创建，否则直接返回已经创建好的
+	{
+		pinstance = new Singleton1(name, holder, rec);
+	}
+	return pinstance;
+}
+
+
+void singletoncreat()
+{
+	std::cout << "记录创建" << std::endl;
+	Singleton* singleton = Singleton::GetInstance("男子100米", "博尔特", "9秒58");
+	singleton->recorddeclaration();
+	std::cout << std::endl;
+	Singleton1* singleton1 = Singleton1::GetInstance("男子200米", "博尔特", "19秒19");
+	singleton1->recorddeclaration();
+
+	std::cout << std::endl << std::endl << std::endl;
+}
+void singletonquery()
+{
+	std::cout << "记录查询" << std::endl;
+	Singleton* singl = Singleton::GetInstance("", "", "");
+	singl->recorddeclaration();
+	std::cout << std::endl;
+	Singleton1* singl1 = Singleton1::GetInstance("", "", "");
+	singl1->recorddeclaration();
+
+}
+
 void singletonTest() {//测试函数
 
 
 	std::cout << "*****************Singleton(单例)设计模式**********" << std::endl << std::endl;
-
-	std::cout << "记录创建" << std::endl;
-	Singleton* singleton = Singleton::GetInstance("男子100米", "博尔特", "9秒58");
-	singleton->recorddeclaration();
-
-	std::cout << std::endl << std::endl << std::endl;
-	std::cout << "记录查询" << std::endl;
-	Singleton* singl = Singleton::GetInstance("", "", "");
-	singl->recorddeclaration();
-
+	singletoncreat();
+	singletonquery();
+	
 	std::cout << "**************************************************" << std::endl << std::endl;
 }
 
