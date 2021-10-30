f1981207e27a8471014de57a6b02a6932c28077e
Haotian Wu <3233990645@qq.com>
2021-10-30 20:06:09 +0800

    前一个提交里忘记交的.....

--- FactoryMethod/factoryMethod.h
+++ FactoryMethod/factoryMethod.h
@@ -22,11 +22,36 @@ void factoryMethodTest(){
     EventsCreator* creator2 = new DivingEventsCreator();
     std::cout << "执行跳水比赛流程：" << std::endl;
     creator2->EventsOperation();
-    
+    std::cout << std::endl << "***************  FactoryMethod(工厂方法)设计模式结束  **************************************" << std::endl << std::endl;
+    delete creator1, creator2;
+}
+
+void factoryMethodTest1() {
+    std::cout << "***************  以下为FactoryMethod(工厂方法)设计模式  **************************************" << std::endl << std::endl;
+    EventsCreator* creator1 = new PingpongEventsCreator();
+    std::cout << "执行乒乓球比赛流程：" << std::endl;
+    creator1->EventsOperation();
     std::cout << std::endl << "***************  FactoryMethod(工厂方法)设计模式结束  **************************************" << std::endl << std::endl;
     delete creator1;
-    
 }
+
+void factoryMethodTest2() {
+    std::cout << "***************  以下为FactoryMethod(工厂方法)设计模式  **************************************" << std::endl << std::endl;
+    EventsCreator* creator2 = new DivingEventsCreator();
+    std::cout << "执行跳水比赛流程：" << std::endl;
+    creator2->EventsOperation();
+    std::cout << std::endl << "***************  FactoryMethod(工厂方法)设计模式结束  **************************************" << std::endl << std::endl;
+    delete creator2;
+}
+void factoryMethodTest3() {
+    std::cout << "***************  以下为FactoryMethod(工厂方法)设计模式  **************************************" << std::endl << std::endl;
+    EventsCreator* creator3 = new BadmintonEventsCreator();
+    std::cout << "执行羽毛球比赛流程：" << std::endl;
+    creator3->EventsOperation();
+    std::cout << std::endl << "***************  FactoryMethod(工厂方法)设计模式结束  **************************************" << std::endl << std::endl;
+    delete creator3;
+}
+
 
 
 #endif /* factoryMethod_h */
