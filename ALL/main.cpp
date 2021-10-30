bfaaad2a011fc5c5c6ab3ab3b5fbcad2e149b43f
Haotian Wu <3233990645@qq.com>
2021-10-30 20:04:30 +0800

    添加 比赛输出 和 奖牌榜

--- ALL/main.cpp
+++ ALL/main.cpp
@@ -2,20 +2,26 @@
 //#include "../Visitor/grantVisitorTest.h"
 //#include "../Proxy/proxy.h"
 //#include "../Memento/memento.h"
-#include"../Observer/Observer.h"
+#include"Observer/Observer.h"
+#include "MVC/MVC.h"
+#include "FactoryMethod/factoryMethod.h"
 #include <iostream>
 #include <stdlib.h>
 
 void menuRole();
+
 void menuAthlete();
+
 void menuWorker();
+
 void menuCostomer();
+
 void StartGame();
+
 void MenuEnd();
 
 void menuMain() {
-    while (1)
-    {
+    while (1) {
         std::cout << "\n\n---------------欢迎进入奥林匹克运动会！---------------\n";
         std::cout << "1.选择用户类型\n";
         std::cout << "2.进行比赛\n";
@@ -23,26 +29,26 @@ void menuMain() {
         int type;
         std::cout << "input:";
         std::cin >> type;
-        switch (type)
-        {
-        case 1:
-            menuRole();
-            break;
-        case 2:
-            //进行比赛
-            StartGame();
-            break;
-        case 0:
-            return;
-            break;
-        default:
-            break;
+        switch (type) {
+            case 1:
+                menuRole();
+                break;
+            case 2:
+                //进行比赛
+                StartGame();
+                break;
+            case 0:
+                return;
+                break;
+            default:
+                break;
         }
         if (type == 2 || type == 0)
             break;
     }
     return;
 }
+
 void menuRole() {
     std::cout << "\n---------------请选择用户类型---------------\n";
     std::cout << "1.运动员\n";
@@ -52,28 +58,28 @@ void menuRole() {
     int type;
     std::cout << "input:";
     std::cin >> type;
-    switch (type)
-    {
-    case 1:
-        //setPersonnel(1);//TemplateMethod
-        //grantVisitorTest(1);//VisitorMethod
-        menuAthlete();
-        break;
-    case 2:
-        //setPersonnel(2);//TemplateMethod
-        //grantVisitorTest(2);//VisitorMethod
-        menuWorker();
-        break;
-    case 3:
-        //setPersonnel(3);//TemplateMethod
-        //grantVisitorTest(3);//VisitorMethod
-        menuCostomer();
-        break;
-    default:
-        break;
+    switch (type) {
+        case 1:
+            //setPersonnel(1);//TemplateMethod
+            //grantVisitorTest(1);//VisitorMethod
+            menuAthlete();
+            break;
+        case 2:
+            //setPersonnel(2);//TemplateMethod
+            //grantVisitorTest(2);//VisitorMethod
+            menuWorker();
+            break;
+        case 3:
+            //setPersonnel(3);//TemplateMethod
+            //grantVisitorTest(3);//VisitorMethod
+            menuCostomer();
+            break;
+        default:
+            break;
     }
     return;
 }
+
 void menuAthlete() {
     while (1) {
         std::cout << "\n---------------请您要进行的操作---------------\n";
@@ -85,28 +91,28 @@ void menuAthlete() {
         int type;
         std::cout << "input:";
         std::cin >> type;
-        switch (type)
-        {
-        case 1:
-            //报名
-            break;
-        case 2:
-            //比赛信息查询
-            break;
-        case 3:
-            //纪念品预定
-            break;
-        case 4:
-            //发起请求
-            break;
-        case 0:
-            break;
+        switch (type) {
+            case 1:
+                //报名
+                break;
+            case 2:
+                //比赛信息查询
+                break;
+            case 3:
+                //纪念品预定
+                break;
+            case 4:
+                //发起请求
+                break;
+            case 0:
+                break;
         }
         if (type == 0)
             break;
     }
     return;
 }
+
 void menuWorker() {
     while (1) {
         std::cout << "\n---------------请您要进行的操作---------------\n";
@@ -120,35 +126,35 @@ void menuWorker() {
         int type;
         std::cout << "input:";
         std::cin >> type;
-        switch (type)
-        {
-        case 1:
-            //提交运动员数据
-            break;
-        case 2:
-            //添加部门
-            break;
-        case 3:
-            //添加职工
-            break;
-        case 4:
-            //更改体育项目场地
-            //observerTest();
-            break;
-        case 5:
-            //查看比赛信息
-            break;
-        case 6:
-            //安排比赛次序
-            break;
-        case 0:
-            break;
+        switch (type) {
+            case 1:
+                //提交运动员数据
+                break;
+            case 2:
+                //添加部门
+                break;
+            case 3:
+                //添加职工
+                break;
+            case 4:
+                //更改体育项目场地
+                //observerTest();
+                break;
+            case 5:
+                //查看比赛信息
+                break;
+            case 6:
+                //安排比赛次序
+                break;
+            case 0:
+                break;
         }
         if (type == 0)
             break;
     }
     return;
 }
+
 void menuCostomer() {
     while (1) {
         std::cout << "\n---------------请您要进行的操作---------------\n";
@@ -158,22 +164,22 @@ void menuCostomer() {
         int type;
         std::cout << "input:";
         std::cin >> type;
-        switch (type)
-        {
-        case 1:
-            //入场
-            break;
-        case 2:
-            //纪念品预定
-            break;
-        case 0:
-            break;
+        switch (type) {
+            case 1:
+                //入场
+                break;
+            case 2:
+                //纪念品预定
+                break;
+            case 0:
+                break;
         }
         if (type == 0)
             break;
     }
     return;
 }
+
 void StartGame() {
     std::cout << "\n\n---------------奥林匹克运动会正式开始！---------------\n\n";
     std::cout << "\n--------本次奥林匹克运动会的比赛项目有-----------\n";
@@ -183,20 +189,23 @@ void StartGame() {
     std::cout << "\n-------接下来请观众进行审核并入场进入观众席-----------\n";
     //观众审核流程输出
     std::cout << "\n--------比赛正式开始--------------\n";
-    std::cout << "\n--------首先进行XXX比赛--------------\n";
+    std::cout << "\n--------首先进行乒乓球比赛--------------\n";
     //XXX比赛输出
-    std::cout << "\n--------接下来进行XXX比赛--------------\n";
+    factoryMethodTest1();
+    std::cout << "\n--------接下来进行跳水比赛--------------\n";
     //XXX比赛输出
-    std::cout << "\n--------接下来进行XXX比赛--------------\n";
+    factoryMethodTest2();
+    std::cout << "\n--------接下来进行羽毛球比赛--------------\n";
     //XXX比赛输出
+    factoryMethodTest3();
     std::cout << "\n--------最后进行XXX比赛--------------\n";
     //XXX比赛输出
     std::cout << "\n--------所有比赛项目顺利结束--------------\n";
     MenuEnd();
 }
+
 void MenuEnd() {
-    while (1)
-    {
+    while (1) {
         std::cout << "\n\n-----------奥林匹克结束菜单-------------\n\n";
         std::cout << "1.打印奖牌榜\n";
         std::cout << "2.打印报告\n";
@@ -204,30 +213,31 @@ void MenuEnd() {
         int type;
         std::cout << "input:";
         std::cin >> type;
-        switch (type)
-        {
-        case 1:
-            //打印奖牌榜
-           // PrintMedalTally();
-            break;
-        case 2:
-            //打印报告
-            // PrintReport();
-            break;
-        case 0:
-            return;
-            break;
-        default:
-            break;
+        switch (type) {
+            case 1:
+                //打印奖牌榜
+                // PrintMedalTally();
+                MVCTest();
+                break;
+            case 2:
+                //打印报告
+                // PrintReport();
+                break;
+            case 0:
+                return;
+                break;
+            default:
+                break;
         }
         if (type == 0)
             break;
     }
     return;
 }
-int main(int argc, const char* argv[]) {
+
+int main(int argc, const char *argv[]) {
     menuMain();
     // Subject temp1;
-     //temp1.Notify();
+    //temp1.Notify();
     return 0;
 }
